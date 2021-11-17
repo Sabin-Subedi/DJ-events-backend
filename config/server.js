386fc343027c789e1a1a71c181ed1d955910d807
Sabin Subedi <69413078+Sabin-Subedi@users.noreply.github.com>
2021-11-17 14:37:23 +0545

    Initial setup of strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '14e5c533379151564e9816ad2e253969'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '00a688c0-3592-4f83-9bfe-71c1cb4039ee'
+};
